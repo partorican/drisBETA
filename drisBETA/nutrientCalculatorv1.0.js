12e5f8a7be8568f2dc5dbb6bf5cc059e33e3192c
partorican <ibringwater@gmail.com>
2022-09-28 09:59:28 -0600

    Second commit 9/28/22

--- drisBETA/nutrientCalculatorv1.0.js
+++ drisBETA/nutrientCalculatorv1.0.js
@@ -4,8 +4,8 @@ const calc = props => {
 return (
 <div>
 <div>{props.currentCounting}</div>
-<div onClick={() => props.setCounting(props.currentCounting+1)}>click me for count up</div>
-<div onClick={() => props.setCounting(props.currentCounting-1)}>click me for count down</div>
+<div onClick={() => props.setCounting(props.currentCounting+1)}>Click me for count up</div>
+<div onClick={() => props.setCounting(props.currentCounting-1)}>Click me for count down</div>
 
 </div>
 )
